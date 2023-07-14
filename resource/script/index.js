541eeeaf6d8a5375b40640341dab3384d3326664
hsyoon <hyungseock.yun@aekyung.kr>
2023-07-14 16:47:17 +0900

    - 랜덤런치 테스트

---
+++ resource/script/index.js
@@ -0,0 +1,39 @@
+if (history.scrollRestoration) {
+  window.history.scrollRestoration = "manual";
+}
+
+const introTitle = document.getElementsByClassName("intro__title")[0];
+const introButton = document.getElementsByClassName("intro__button")[0];
+const game = document.getElementById("game");
+
+// 타이틀 문구 타이핑
+const title = "랜덤런치 13조\n후기\n시작합니다";
+
+let cnt = 0;
+let timer = 0;
+
+function typingIntroTitle() {
+  let character = title[cnt++];
+
+  if (character === "\n") {
+    introTitle.innerHTML = introTitle.innerHTML + "<br/>";
+  } else {
+    introTitle.innerHTML = introTitle.innerHTML + character;
+  }
+
+  if (cnt === title.length) {
+    clearInterval(timer);
+    introButton.classList.add("show");
+
+    return;
+  }
+}
+
+// 버튼 클릭 시 게임 목록으로 이동
+introButton.addEventListener('click', () => {
+  game.scrollIntoView({behavior: "smooth"});
+});
+
+window.onload = function () {
+  timer = setInterval(typingIntroTitle, 200);
+}
