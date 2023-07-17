e98813d958ca19e5355023f307b3b03df08ced91
hsyoon <hyungseock.yun@aekyung.kr>
2023-07-17 14:18:23 +0900

    - 사진 목록 기능 추가

---
+++ resource/script/photo.js
@@ -0,0 +1,25 @@
+let photoIdx = 0;
+let slider;
+window.onload = function () {
+  slider = document.querySelectorAll(".photo_img");
+}
+
+function moveToPre() {
+  let preIdx = photoIdx - 1;
+  let idx = (preIdx < 0) ? slider.length - 1 : preIdx;
+  changeNode(photoIdx, idx);
+}
+
+function moveToNext() {
+  let nextIdx = photoIdx + 1;
+  let idx = (nextIdx > slider.length - 1) ? 0 : nextIdx;
+  changeNode(photoIdx, idx);
+}
+
+function changeNode(mIdx, idx) {
+  let nowNode = slider[mIdx];
+  let nextNode = slider[idx];
+  nowNode.style.display = 'none';
+  nextNode.style.display = 'inline-flex';
+  photoIdx = idx;
+}
