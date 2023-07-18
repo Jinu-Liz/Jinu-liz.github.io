8edcbb96c638ec42096525491601601723bdad45
hsyoon <hyungseock.yun@aekyung.kr>
2023-07-18 10:42:36 +0900

    - 제작자 콘솔 추가 - 후기 스크립트 추가

---
+++ resource/script/evaluation.js
@@ -0,0 +1,25 @@
+let memberIdx = 0;
+let slider;
+window.onload = function () {
+  slider = document.querySelectorAll(".game__board");
+}
+
+function moveToPre() {
+  let preIdx = memberIdx - 1;
+  let idx = (preIdx < 0) ? slider.length - 1 : preIdx;
+  changeNode(memberIdx, idx);
+}
+
+function moveToNext() {
+  let nextIdx = memberIdx + 1;
+  let idx = (nextIdx > slider.length - 1) ? 0 : nextIdx;
+  changeNode(memberIdx, idx);
+}
+
+function changeNode(mIdx, idx) {
+  let nowNode = slider[mIdx];
+  let nextNode = slider[idx];
+  nowNode.style.display = 'none';
+  nextNode.style.display = 'inline-flex';
+  memberIdx = idx;
+}
